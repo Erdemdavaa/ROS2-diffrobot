082c4dcfd2d1b4109dab27ad3815be6d00bded00
Erdemdavaa <mherodavaa@gmail.com>
2025-11-04 16:37:48 +0100

    Fiz Gazebo include launch - corrected path so robot spawns properly

--- diffrobot_simulation/launch/gz_include_launch.py
+++ diffrobot_simulation/launch/gz_include_launch.py
@@ -1,32 +1,31 @@
 import os
 from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
-    
+    # Locate Gazebo (ros_gz_sim) and your world
     ros_gz_sim_dir = get_package_share_directory('ros_gz_sim')
-    ros_gz_launch_dir = os.path.join(ros_gz_sim_dir, 'launch')
-
     world_pkg_name = 'diffrobot_simulation'
-    world_file_subpath = 'worlds/diffrobot_example_world.sdf'
-    world_file = os.path.join(get_package_share_directory(world_pkg_name),world_file_subpath)
+    world_file = os.path.join(
+        get_package_share_directory(world_pkg_name),
+        'worlds',
+        'diffrobot_example_world.sdf'
+    )
 
+    # Launch Gazebo with your world
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim_dir, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={
+            'gz_args': f'-r {world_file}'
+        }.items(),
+    )
 
     return LaunchDescription([
-        IncludeLaunchDescription(
-            #PathJoinSubstitution(os.path.join(ros_gz_launch_dir, 'gz_sim.launch.py')),
-            PathJoinSubstitution([
-                FindPackageShare('ros_gz_sim'),
-                'launch',
-                'gz_sim.launch.py'
-            ]),
-            launch_arguments={
-                'gz_args': ['', world_file],
-                'on_exit_shutdown' : 'true'
-            }.items(),
-        )
+        gazebo
     ])
